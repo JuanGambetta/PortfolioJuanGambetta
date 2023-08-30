bdf859ed068403748dedf38b9546a9f6afeb9f06
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2023-08-30 20:04:04 -0300

    arreglado

--- Ut1-Bootstrap/src/script.js
+++ Ut1-Bootstrap/src/script.js
@@ -1,27 +1,27 @@
-const agregarProyBut = document.getElementById('agregarProyectos');
-agregarProyBut.addEventListener('click', handleClickProy);
+const agregarProyBut = document.getElementById('agregarProyectos')
+agregarProyBut.addEventListener('click', handleClickProy)
 
-const infoBut = document.getElementById('info');
-infoBut.addEventListener('click', handleClicki);
+const infoBut = document.getElementById('info')
+infoBut.addEventListener('click', handleClicki)
 
-const notifBut = document.getElementById('noti');
-notifBut.addEventListener('click', handleClickn);
+const notifBut = document.getElementById('noti')
+notifBut.addEventListener('click', handleClickn)
 
-const handleHoverP = document.getElementById('personas');
-handleHoverP.addEventListener('mouseover', handleHoverPF);
+const handleHoverP = document.getElementById('personas')
+handleHoverP.addEventListener('mouseover', handleHoverPF)
 
-const handleHoverP2 = document.getElementById('personas');
-handleHoverP2.addEventListener('mouseout', handleHoverPF);
+const handleHoverP2 = document.getElementById('personas')
+handleHoverP2.addEventListener('mouseout', handleHoverPF)
 
-const link = 'https://jsonplaceholder.typicode.com/users';
+const link = 'https://jsonplaceholder.typicode.com/users'
 
-let boolProy = true;
+let boolProy = true
 
-let cardCount = 1;
+let cardCount = 1
 
-let dropdownContent = document.getElementById('dropdownPersonas');
+let dropdownContent = document.getElementById('dropdownPersonas')
 
-let arrayPersonas = [];
+let arrayPersonas = []
 
 fetchData(link)
 
@@ -29,32 +29,32 @@ function handleClickProy() {
     if (confirm('Quieres agregar un nuevo proyecto?')) {
         
         const nombreProyecto = prompt('Ingresa el nombre del proyecto')
-        const newProy = document.createElement('div');
-        const newBody = document.createElement('div');
-        const tituloProyecto = document.createElement('h5');
-        const newButton = document.createElement('a');
-        const newTareas = document.createElement('div');
+        const newProy = document.createElement('div')
+        const newBody = document.createElement('div')
+        const tituloProyecto = document.createElement('h5')
+        const newButton = document.createElement('a')
+        const newTareas = document.createElement('div')
 
-        newProy.classList.add('card');
-        newBody.classList.add('card-body');
-        tituloProyecto.classList.add('card-title');
-        tituloProyecto.textContent = nombreProyecto;
-        newButton.classList.add('btn');
-        newButton.classList.add('btn-primary');
-        newButton.classList.add('bot');
-        newButton.hrefList = '#';
-        newButton.id = 'agregarTarea' + cardCount;
-        newButton.textContent = 'Nueva Tarea';
+        newProy.classList.add('card')
+        newBody.classList.add('card-body')
+        tituloProyecto.classList.add('card-title')
+        tituloProyecto.textContent = nombreProyecto
+        newButton.classList.add('btn')
+        newButton.classList.add('btn-primary')
+        newButton.classList.add('bot')
+        newButton.hrefList = '#'
+        newButton.id = 'agregarTarea' + cardCount
+        newButton.textContent = 'Nueva Tarea'
         newButton.style = "width: 95%"
-        newTareas.id = 'tareasProy' + cardCount;
-        cardCount += 1;
+        newTareas.id = 'tareasProy' + cardCount
+        cardCount += 1
 
-        agregarListener(newButton);
+        agregarListener(newButton)
 
-        newProy.appendChild(newBody);
-        newBody.appendChild(tituloProyecto);
-        newBody.appendChild(newTareas);
-        newBody.appendChild(newButton);
+        newProy.appendChild(newBody)
+        newBody.appendChild(tituloProyecto)
+        newBody.appendChild(newTareas)
+        newBody.appendChild(newButton)
 
         if (boolProy) {
             const slot1 = document.getElementById('slot1')
@@ -82,25 +82,25 @@ function agregarTareafun(event){
     const nombreTarea = prompt('Ingresa el nombre de la tarea')
     const asignarTarea = document.createElement('button')
     asignarTarea.setAttribute("style", "height: 26px; width: 36px;") 
-    asignarTarea.classList.add('botonTarea');
-    asignarTareafun(asignarTarea);
-    let idBien = event.target.id;
+    asignarTarea.classList.add('botonTarea')
+    asignarTareafun(asignarTarea)
+    let idBien = event.target.id
     idBien = idBien[idBien.length - 1]
-    idBien = 'tareasProy' + idBien;
-    const contTareas = document.getElementById(idBien);
-    const nuevaTarea = document.createElement('p');
-    nuevaTarea.classList.add('card-text');
-    nuevaTarea.textContent = nombreTarea;
+    idBien = 'tareasProy' + idBien
+    const contTareas = document.getElementById(idBien)
+    const nuevaTarea = document.createElement('p')
+    nuevaTarea.classList.add('card-text')
+    nuevaTarea.textContent = nombreTarea
 
-    const contenedor = document.createElement('div');
+    const contenedor = document.createElement('div')
     contenedor.style = 'display: flex; flex-direction: row; justify-content: space-between;'
-    contenedor.appendChild(nuevaTarea);
-    contenedor.appendChild(asignarTarea);
-    contTareas.appendChild(contenedor);
+    contenedor.appendChild(nuevaTarea)
+    contenedor.appendChild(asignarTarea)
+    contTareas.appendChild(contenedor)
 }
 function agregarListener(element){
     element.addEventListener('click', event => agregarTareafun(event))
-};
+}
 function asignarTareafun(element){
     element.addEventListener('click', event => asignarTareafuncion(event))
 }
@@ -109,29 +109,28 @@ function asignarTareafuncion(event){
     if (arrayPersonas.includes(posibleTexto)){
             posibleTexto = posibleTexto.split(" ")
             posibleTexto = posibleTexto[0][0] + posibleTexto[1][0]
-            console.log(posibleTexto);
+            console.log(posibleTexto)
             event.target.textContent = posibleTexto
     }else { alert(`No existe una persona con el nombre ` + posibleTexto)
     }
 }
-
 function fetchData() {
     fetch(link, { method: "GET" })
       .then((response) => {
         return response.json(); // extract JSON from response
       })
       .then((data) => {
-        let counter = 0;
+        let counter = 0
         data.forEach(element => {
             arrayPersonas[counter] = element.name
-            counter += 1;
-            let elemento = document.createElement('p');
-            elemento.innerHTML = element.name;
-            elemento.classList.add('dropeable');
+            counter += 1
+            let elemento = document.createElement('p')
+            elemento.innerHTML = element.name
+            elemento.classList.add('dropeable')
             dropdownContent.appendChild(elemento)
-        });
+        })
       })
       .catch((error) => {
-        console.log("Error fetching data: ", error);
-      });
-  }
+        console.log("Error fetching data: ", error)
+      })
+}
